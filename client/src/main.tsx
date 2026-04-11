dd1e683efe16c359594c9734b4c6bb6b315004bf
Erruuu <64939964+EruBot@users.noreply.github.com>
2026-04-12 03:22:00 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// 🔥 HANDLE REDIRECT DARI 404.HTML
+const redirect = sessionStorage.getItem("redirect");
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
